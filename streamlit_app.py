d6e7fe5f376d560560d82c58dfe84e0bb428361e
Syairaza <syairaza9@gmail.com>
2025-05-21 19:29:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("Syaira🤙🏼😛")
+st.title("Iron lady😎🤟🏼")
 st.write(
-    "i'm like some kind of supernova. WATCH OUT!😎"
+    "i'm like some kind of supernova. WATCH OUT!😏"
 )
 st.image("IMG_0391.jpeg", width=200)
