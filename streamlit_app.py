7bee488e89caca37c9ef201e2cf65968a7650da2
Syairaza <syairaza9@gmail.com>
2025-05-21 19:03:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,3 @@ st.title("Selamat datang di Web Informatika")
 st.write(
     "i'm like some kind of supernova. WATCH OUT!😎"
 )
-st.image("https://github.com/Syairaza/latihaninfor/blob/main/IMG_0391.jpeg", width=200)
