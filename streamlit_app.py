dbe02122e2793827a382c7b560e5ddadf241a3a6
Syairaza <syairaza9@gmail.com>
2025-05-21 18:41:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("Selamat datang di Web Informatika")
 st.write(
     "ngodingseru bersama Syaira Zafina Dalimunthe😎"
 )
+st.image("https://github.com/Syairaza/latihaninfor/blob/main/IMG_0391.jpeg", width=200)
